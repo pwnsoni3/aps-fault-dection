117a7cc6a4145bac0a731704041036ac8f1a3080
pawan_kumar_soni <pwnsoni3@gmail.com>
2022-12-03 13:37:20 +0530

    created logging module and basic

---
+++ logger.py
@@ -0,0 +1,24 @@
+
+
+import logging
+import os 
+from datetime import datetime
+import os
+
+# log file time
+LOG_FILE_NAME= f"{detetime.now().strftime('%m%d%Y__%H%M%S')}.log"
+
+#log directory
+LOG_FILE_DIR=os.path.join(os.getcwd(),"logs")
+
+#creat folder if not avalable 
+os.makedirs(LOG_FILE_DIR, exist_ok=True)
+
+#log file path
+LOG_FILE_PATH= os.path.join(LOG_FILE_DIR,LOG_FILE_NAME)
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[%(asctime)s] %(lineno)d %(name)s -%(levelname)s -%(message)s",
+    level=logging.INFO,
+)
